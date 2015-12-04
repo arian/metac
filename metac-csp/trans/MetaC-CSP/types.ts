105ad5ebce171227d0e3eb3d111a988e96645d53
Arian Stolwijk <stolwijk.arian@gmail.com>
2015-12-04 01:20:19 +0100

    Type checking reading/writing to channels.

---
+++ metac-csp/trans/MetaC-CSP/types.ts
@@ -0,0 +1,24 @@
+module MetaC-CSP/types
+
+imports
+
+  signatures/MetaC-CSP-sig
+  BaseC/desugar/constructors
+
+type rules
+
+  CSPChanWrite(chan, expr):-
+    where
+          chan: ct
+      and ct => CSPChan(Type(_, vt))
+      and expr : et
+      and (et <is-assignable: vt)
+        else error $[Can't write value of type: [et] to channel of type [vt]] on expr
+
+  CSPChanRead(chan, dest):-
+    where
+          chan: ct
+      and ct => CSPChan(Type(_, vt))
+      and dest : dt
+      and (vt <is-assignable: dt)
+        else error $[Can't assign a value from the channel with type: [vt] to variable of type [dt]] on dest
