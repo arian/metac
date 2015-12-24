84ad1b25cae3bc694895557cf28d701bb66978fa
Arian Stolwijk <stolwijk.arian@gmail.com>
2015-12-24 14:32:19 +0100

    Add anonymous channel read, which automatically creates a temp variable
    Now
    
      foobar(chan?);
    
    is the same as
    
      int x;
      chan ? x;
      foobar(x);
    
    Also both reads return the variable value (using a comma expression):
    
      (CSP_chanInCopy(chan, &x, sizeof ...), x)
    
    So it's easier to use in expressions directly.

--- metac-csp/trans/MetaC-CSP/types.ts
+++ metac-csp/trans/MetaC-CSP/types.ts
@@ -9,16 +9,18 @@ type rules
 
   CSPChanWrite(_, chan, expr):-
     where
-          chan: ct
-      and ct => CSPChan(Type(_, vt))
+          chan: CSPChan(Type(_, vt))
       and expr : et
       and (et <is-assignable: vt)
         else error $[Can't write value of type: [et] to channel of type [vt]] on expr
 
-  CSPChanRead(chan, dest):-
+  CSPChanRead(chan, dest): vt
     where
-          chan: ct
-      and ct => CSPChan(Type(_, vt))
+          chan: CSPChan(Type(_, vt))
       and dest : dt
       and (vt <is-assignable: dt)
         else error $[Can't assign a value from the channel with type: [vt] to variable of type [dt]] on dest
+
+  CSPAnonChanRead(chan, name): vt
+    where
+      chan: CSPChan(Type(_, vt))
